ab2ce5b975d22043af4b481ca42e6ceab18ae77f
Chrysanthos Rouvellas <75946785+hoolies@users.noreply.github.com>
2023-03-23 19:26:06 -0400

    Update CiscoSSH.py

--- Functions/CiscoSSH.py
+++ Functions/CiscoSSH.py
@@ -1,26 +1,26 @@
-import netmiko
-import getpass
+from netmiko import ConnectHandler, AuthenticationException, NetMikoTimeoutException, SSHException
+from getpass import getpass
 
 username = input("Username: ")
-password = getpass.getpass("Password: ")
+password = getpass("Password: ")
 
 #  SSH function
 def ssh(device, commands):
     try:
         # Connection Handler    
-        connection = netmiko.ConnectHandler(ip=device, device_type="cisco_ios", username=username, password=password, secret=password)
+        connection = ConnectHandler(ip=device, device_type="cisco_ios", username=username, password=password, secret=password)
         connection.enable()
         # Dictionary comprehension for command and output
         command_output = {command:connection.send_command(command) for command in commands}
         connection.disconnect
         return command_output
-    except(netmiko.AuthenticationException):
+    except(AuthenticationException):
         print('Authentication failure: ' + device)
-    except(netmiko.NetMikoTimeoutException):
+    except(NetMikoTimeoutException):
         print('Timeout to device ' + device)
     except(EOFError):
         print("End of file while attempting device " + device)
-    except(netmiko.SSHException):
+    except(SSHException):
         print('SSH might not be enabled for ' + device)
     except Exception as unknown_error:
         print('Uknown error: ' + unknown_error)
