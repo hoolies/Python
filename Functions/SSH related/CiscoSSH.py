4b9fc2723f68dd744e08908de0158716ec51cfed
hoolies <c.rouvellas@gmail.com>
2023-10-05 23:16:37 -0400

    Testing

--- Functions/SSH related/CiscoSSH.py
+++ Functions/SSH related/CiscoSSH.py
@@ -20,3 +20,13 @@ def ssh(device, commands):
         print("End of file while attempting device " + device)
     except Exception as e:
         print('Unknown error: ' + f"{e}")
+
+def main():
+    # Device list
+    devices = input("Enter device list: ").split()
+    # Commands list
+    commands = input("Enter commands list: ").split()
+
+
+if __name__ == '__main__':
+    main()
